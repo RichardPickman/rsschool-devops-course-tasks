928102985887aefb0da098d33ba4eab65f693711
Richard Pickman <drezzerock@gmail.com>
2024-10-02 16:14:28 +0200

    the great return of StringLike with pull request populated in it

--- policies.tf
+++ policies.tf
@@ -12,26 +12,26 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 data "aws_iam_policy_document" "github_assume_role_policy" {
-  version = "2012-10-17"
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = [
-        "repo:richardpickman/rsschool-devops-course-tasks:ref:refs/heads/main",
+    version = "2012-10-17"
+    statement {
+        actions = ["sts:AssumeRoleWithWebIdentity"]
+        principals {
+            type        = "Federated"
+            identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+        }
+        condition {
+            test     = "StringEquals"
+            variable = "token.actions.githubusercontent.com:aud"
+            values   = ["sts.amazonaws.com"]
+        }
+        condition {
+            test     = "StringLike"
+            variable = "token.actions.githubusercontent.com:sub"
+            values = [
+              "repo:richardpickman/rsschool-devops-course-tasks:ref:refs/heads/main",
         "repo:richardpickman/rsschool-devops-course-tasks:pull_request",
-      ]
-
+            ]
+            
+        }
     }
-  }
 }
